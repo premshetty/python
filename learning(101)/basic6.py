32ffb554cc00396528a1853e63e8a31d1e4f3ff3
premshetty <premenaga@gmail.com>
2023-07-27 11:04:08 +0530

    list and tuples

--- learning(101)/basic6.py
+++ learning(101)/basic6.py
@@ -1,1 +1,26 @@
 # list and tuples
+
+# tuples, immutable
+
+x = (0,1,2,3,4)
+print(x[0]) 
+
+# list , mutable
+
+a = [1,2,'gh', True]
+
+# make a deep copy
+b = a[:]
+print(a[1])
+print(len(a))
+a.extend([1,2,3,4,'e',5])
+print(a.pop())
+print(a.remove(2))
+print(a)
+
+print(a.clear())
+print(a)
+
+print(b)
+
+
