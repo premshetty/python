1eb4fb1be42bbef34275f5f8da3e6ab6c75346d8
premshetty <premenaga@gmail.com>
2023-07-27 10:39:44 +0530

    conditonal statement

---
+++ learning(101)/basic5.py
@@ -0,0 +1,14 @@
+# if else 
+
+
+name = input('Name: ')
+age = input('Age:')
+
+if name=="prem":
+    print('Hi Prem')
+    if int(age) > 10:
+        print('good age')
+elif name=="bye":
+    print('bye')
+else :
+    print("I don't know you")
